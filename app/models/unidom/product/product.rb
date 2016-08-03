b3612294332820a8ee3d088bcfbfcf6bd474a450
Topbit Du <topbit.du@gmail.com>
2016-08-03 23:18:28 +0800

    1, Improve the Product model for the coding style.

--- app/models/unidom/product/product.rb
+++ app/models/unidom/product/product.rb
@@ -4,11 +4,11 @@ class Unidom::Product::Product < ActiveRecord::Base
 
   self.table_name = 'unidom_products'
 
+  include Unidom::Common::Concerns::ModelExtension
+
   validates :name,             presence: true, length: { in: 2..self.columns_hash['name'].limit             }
   validates :abbreviation,     presence: true, length: { in: 1..self.columns_hash['abbreviation'].limit     }
   validates :measurement_unit, presence: true, length: { in: 1..self.columns_hash['measurement_unit'].limit }
   validates :packing_norm,     presence: true, length: { in: 1..self.columns_hash['packing_norm'].limit     }
-
-  include Unidom::Common::Concerns::ModelExtension
 
 end
