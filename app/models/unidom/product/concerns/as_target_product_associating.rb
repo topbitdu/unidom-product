ee5b568d1b2fb243bbe893b98945b626a3764a09
Topbit Du <topbit.du@gmail.com>
2016-09-29 11:21:08 +0800

    1, Improve the As Target Product Associating concern to add the #is_associated! method.

--- app/models/unidom/product/concerns/as_target_product_associating.rb
+++ app/models/unidom/product/concerns/as_target_product_associating.rb
@@ -7,6 +7,14 @@ module Unidom::Product::Concerns::AsTargetProductAssociating
     has_many :source_product_associatings, class_name: 'Unidom::Product::ProductAssociating', foreign_key: :target_id
     has_many :source_products,             through:    :source_product_associatings,          source:      :source
 
+    #def is_associated?(source, due_to: nil, at: Time.now)
+    #  source_product_associatings.source_is(source).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
+    #end
+
+    def is_associated!(source, due_to: nil, at: Time.now, ordinal: 1)
+      source_product_associatings.source_is(source).product_association_coded_as(due_to).valid_at(now: at).alive.first_or_create! ordinal: ordinal, opened_at: at
+    end
+
   end
 
   module ClassMethods
