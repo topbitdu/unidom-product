91678f804325cf3fde09851605cdc339b41fc755
Topbit Du <topbit.du@gmail.com>
2016-10-01 07:50:42 +0800

    1, Improve the As Source Product Associating concern to add the #associate? method.

--- app/models/unidom/product/concerns/as_source_product_associating.rb
+++ app/models/unidom/product/concerns/as_source_product_associating.rb
@@ -7,9 +7,9 @@ module Unidom::Product::Concerns::AsSourceProductAssociating
     has_many :target_product_associatings, class_name: 'Unidom::Product::ProductAssociating', foreign_key: :source_id
     has_many :target_products,             through:    :target_product_associatings,          source: :target
 
-    #def associate?(target, due_to: nil, at: Time.now)
-    #  target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
-    #end
+    def associate?(target, due_to: nil, at: Time.now)
+      target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
+    end
 
     def associate!(target, due_to: nil, at: Time.now, ordinal: 1)
       target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.first_or_create! ordinal: ordinal, opened_at: at
