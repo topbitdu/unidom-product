4274a909043da9ea7c6eca5ad38608f84e7a4db6
Topbit Du <topbit.du@gmail.com>
2016-10-08 07:37:17 +0800

    1, Deprecate the As Source Product Associating concern.

--- app/models/unidom/product/concerns/as_source_product_associating.rb
+++ app/models/unidom/product/concerns/as_source_product_associating.rb
@@ -8,10 +8,12 @@ module Unidom::Product::Concerns::AsSourceProductAssociating
     has_many :target_products,             through:    :target_product_associatings,          source: :target
 
     def associate?(target, due_to: nil, at: Time.now)
+      warn "The Unidom::Product::Concerns::AsSourceProductAssociating#associate? method is deprecated and will be removed from unidom-product v2.0. Please use the Unidom::Product::Concerns::AsSourceProduct#associate? method instead."
       target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
     end
 
     def associate!(target, due_to: nil, at: Time.now, ordinal: 1)
+      warn "The Unidom::Product::Concerns::AsSourceProductAssociating#associate! method is deprecated and will be removed from unidom-product v2.0. Please use the Unidom::Product::Concerns::AsSourceProduct#associate! method instead."
       target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.first_or_create! ordinal: ordinal, opened_at: at
     end
 
