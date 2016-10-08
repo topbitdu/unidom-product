e0c9071f79391495a03cd29b19d62d00b27bcd82
Topbit Du <topbit.du@gmail.com>
2016-10-08 09:55:51 +0800

    1, Deprecate the As Target Product Associating concern.

--- app/models/unidom/product/concerns/as_target_product_associating.rb
+++ app/models/unidom/product/concerns/as_target_product_associating.rb
@@ -8,10 +8,12 @@ module Unidom::Product::Concerns::AsTargetProductAssociating
     has_many :source_products,             through:    :source_product_associatings,          source:      :source
 
     def is_associated?(source, due_to: nil, at: Time.now)
+      warn "The Unidom::Product::Concerns::AsTargetProductAssociating#is_associated? method is deprecated and will be removed from unidom-product v2.0. Please use the Unidom::Product::Concerns::AsTargetProduct#is_associated? method instead."
       source_product_associatings.source_is(source).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
     end
 
     def is_associated!(source, due_to: nil, at: Time.now, ordinal: 1)
+      warn "The Unidom::Product::Concerns::AsTargetProductAssociating#is_associated! method is deprecated and will be removed from unidom-product v2.0. Please use the Unidom::Product::Concerns::AsTargetProduct#is_associated! method instead."
       source_product_associatings.source_is(source).product_association_coded_as(due_to).valid_at(now: at).alive.first_or_create! ordinal: ordinal, opened_at: at
     end
 
