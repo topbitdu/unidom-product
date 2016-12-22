4d9bb26a095c6ce69ef56fa02b9d8ef1783d45dc
Topbit Du <topbit.du@gmail.com>
2016-12-22 09:24:26 +0800

    1, Improve the Product model to configure the Formset enum code.

--- app/models/unidom/product/product.rb
+++ app/models/unidom/product/product.rb
@@ -7,10 +7,13 @@ class Unidom::Product::Product < Unidom::Product::ApplicationRecord
   include Unidom::Common::Concerns::ModelExtension
   include Unidom::Product::Concerns::AsSourceProduct
   include Unidom::Product::Concerns::AsTargetProduct
+  include ProgneTapera::EnumCode
 
   validates :name,             presence: true, length: { in: 2..self.columns_hash['name'].limit             }
   validates :abbreviation,     presence: true, length: { in: 1..self.columns_hash['abbreviation'].limit     }
   validates :measurement_unit, presence: true, length: { in: 1..self.columns_hash['measurement_unit'].limit }
   validates :packing_norm,     presence: true, length: { in: 1..self.columns_hash['packing_norm'].limit     }
+
+  code :formset, Unidom::Product::Formset
 
 end
