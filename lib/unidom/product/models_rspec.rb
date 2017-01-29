866a4f89e6cfc36a7ee637dd05aa29058c27d8b5
Topbit Du <topbit.du@gmail.com>
2017-01-29 10:33:20 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/product/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/product/product_spec'             unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Product::Product'
+require 'rspec/models/unidom/product/product_associating_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Product::ProductAssociating'
