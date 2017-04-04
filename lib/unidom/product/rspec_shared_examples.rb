9add404ad098703ce78fe153343da9a768a786cd
Topbit Du <topbit.du@gmail.com>
2017-04-05 07:41:44 +0800

    1, Improve the RSpec shared examples manifest to remove the As Source Product shared examples & the As Target Product shared examples.

--- lib/unidom/product/rspec_shared_examples.rb
+++ lib/unidom/product/rspec_shared_examples.rb
@@ -1,1 +1,2 @@
-require 'rspec/models/unidom/product/concerns/as_source_product_shared_examples'
+#require 'rspec/models/unidom/product/concerns/as_source_product_shared_examples'
+#require 'rspec/models/unidom/product/concerns/as_target_product_shared_examples'
