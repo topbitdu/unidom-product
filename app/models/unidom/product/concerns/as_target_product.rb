b06a3086c82ae89ef574f0302dff819838d476ee
Topbit Du <topbit.du@gmail.com>
2017-04-10 09:46:15 +0800

    1, Improve the As Target Product concern for the argument validations of the #is_associated! method, & the #is_associated? method.

--- app/models/unidom/product/concerns/as_target_product.rb
+++ app/models/unidom/product/concerns/as_target_product.rb
@@ -3,7 +3,8 @@
 
 module Unidom::Product::Concerns::AsTargetProduct
 
-  extend ActiveSupport::Concern
+  extend  ActiveSupport::Concern
+  include Unidom::Common::Concerns::ArgumentValidation
 
   included do |includer|
 
@@ -11,11 +12,24 @@ module Unidom::Product::Concerns::AsTargetProduct
     has_many :source_products,             through:    :source_product_associatings,          source:      :source
 
     def is_associated?(source, due_to: nil, at: Time.now)
+
+      assert_present! :source, source
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+
       source_product_associatings.source_is(source).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
+
     end
 
     def is_associated!(source, due_to: nil, at: Time.now, ordinal: 1)
+
+      assert_present! :source, source
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+      assert_present! :ordinal, ordinal
+
       source_product_associatings.source_is(source).product_association_coded_as(due_to).valid_at(now: at).alive.first_or_create! ordinal: ordinal, opened_at: at
+
     end
 
   end
