e1cb60c6ed35c12e00803c1f198265815ff26f85
Topbit Du <topbit.du@gmail.com>
2017-04-11 10:07:09 +0800

    1, Improve the As Source Product shared examples for the argument validations of the #associate! method, & the #associate? method.

--- lib/rspec/models/unidom/product/concerns/as_source_product_shared_examples.rb
+++ lib/rspec/models/unidom/product/concerns/as_source_product_shared_examples.rb
@@ -18,6 +18,11 @@ shared_examples 'Unidom::Product::Concerns::AsSourceProduct' do |model_attribute
 
     it_behaves_like 'has_many', model_attributes, :target_product_associatings, Unidom::Product::ProductAssociating, [ target_product_associating_1_attribtues, target_product_associating_2_attribtues ]
 
+    model  = described_class.create! model_attributes
+    target = described_class.create! model_attributes
+    it_behaves_like 'assert_present!', model, :associate!, [ target, { due_to: 'CPLM', at: Time.now } ], [ { 0 => :target }, :due_to, :at ]
+    it_behaves_like 'assert_present!', model, :associate?, [ target, { due_to: 'CPLM', at: Time.now } ], [ { 0 => :target }, :due_to, :at ]
+
   end
 
 end
