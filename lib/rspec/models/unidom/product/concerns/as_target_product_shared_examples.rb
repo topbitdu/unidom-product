0377c34ab919a30ae1b8048c01a01b2a7c2ec4be
Topbit Du <topbit.du@gmail.com>
2017-04-12 10:10:03 +0800

    1, Improve the As Target Product shared examples for the argument validations of the #is_associated! method, & the #is_associated? method.

--- lib/rspec/models/unidom/product/concerns/as_target_product_shared_examples.rb
+++ lib/rspec/models/unidom/product/concerns/as_target_product_shared_examples.rb
@@ -18,6 +18,11 @@ shared_examples 'Unidom::Product::Concerns::AsTargetProduct' do |model_attribute
 
     it_behaves_like 'has_many', model_attributes, :source_product_associatings, Unidom::Product::ProductAssociating, [ source_product_associating_1_attribtues, source_product_associating_2_attribtues ]
 
+    model  = described_class.create! model_attributes
+    source = described_class.create! model_attributes
+    it_behaves_like 'assert_present!', model, :is_associated!, [ source, { due_to: 'CPLM', at: Time.now } ], [ { 0 => :source }, :due_to, :at ]
+    it_behaves_like 'assert_present!', model, :is_associated?, [ source, { due_to: 'CPLM', at: Time.now } ], [ { 0 => :source }, :due_to, :at ]
+
   end
 
 end
