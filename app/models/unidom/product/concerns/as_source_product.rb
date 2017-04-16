68399e806d99b42edd34709c2e02ceb4283af108
Topbit Du <topbit.du@gmail.com>
2017-04-16 13:53:56 +0800

    1, Improve the As Source Product concern for the coding style.

--- app/models/unidom/product/concerns/as_source_product.rb
+++ app/models/unidom/product/concerns/as_source_product.rb
@@ -11,16 +11,6 @@ module Unidom::Product::Concerns::AsSourceProduct
     has_many :target_product_associatings, class_name: 'Unidom::Product::ProductAssociating', foreign_key: :source_id
     has_many :target_products,             through:    :target_product_associatings,          source: :target
 
-    def associate?(target, due_to: nil, at: Time.now)
-
-      assert_present! :target, target
-      assert_present! :due_to, due_to
-      assert_present! :at,     at
-
-      target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
-
-    end
-
     def associate!(target, due_to: nil, at: Time.now, ordinal: 1)
 
       assert_present! :target,  target
@@ -29,6 +19,16 @@ module Unidom::Product::Concerns::AsSourceProduct
       assert_present! :ordinal, ordinal
 
       target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.first_or_create! ordinal: ordinal, opened_at: at
+
+    end
+
+    def associate?(target, due_to: nil, at: Time.now)
+
+      assert_present! :target, target
+      assert_present! :due_to, due_to
+      assert_present! :at,     at
+
+      target_product_associatings.target_is(target).product_association_coded_as(due_to).valid_at(now: at).alive.exists?
 
     end
 
